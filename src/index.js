d0eb05789f343244a699464abc81e5cb50396731
Maximka6699 <ma34lom@gmail.com>
2023-07-21 10:19:53 +0300

    html+react
    добавил в репозиторий сайт на html+css который я верстал вчера.
    так же начал переносить на реакт

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import './style.css'
-import Frame1 from './views/frame1'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Route component={Frame1} exact path="/" />
-      </div>
-    </Router>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
